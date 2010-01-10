c1882716ddf9dfce9dfb7f512b62bbce875fd33f
marius.ion85 <marius.ion85@2c80a768-be1d-11de-82ee-51df2961d3cf>
2010-01-10 10:29:42 +0000

    Ballot class

---
+++ trunk/E-vote/src/voter/Ballot.java
@@ -0,0 +1,33 @@
+package voter;
+
+import java.io.Serializable;
+import java.security.SecureRandom;
+
+public class Ballot implements Serializable {
+
+    private final int       vote_option_id;
+    private final String    candidate;
+    private final String    organization;
+    private final byte []   randomBytes;
+
+    public Ballot( int vote_option_id, String candidate, String organization ) throws Exception {
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG","SUN");
+        this.randomBytes = new byte[16];
+        random.nextBytes(randomBytes);
+        this.vote_option_id = vote_option_id;
+        this.candidate = candidate;
+        this.organization = organization;
+    }
+
+    public String getCandidate() {
+        return candidate;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public int getVote_option_id() {
+        return vote_option_id;
+    }
+}
